07c690235845c3c51d22dd4d7e69a2828b2ff022
zhangying9 <12345678>
2017-07-26 14:42:56 +0800

    代码风格颜色需要修改node_modules里lib下的navbar里的color。

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,37 +1,53 @@
 <template>
   <div class="detail">
     <router-link to="/" class="course-video"></router-link>
-    <div class="like"><span>{{likeNum}}</span></div>
+    <div class="clickLike"><span class="detail-title">{{detailTitle}}</span><span class="likeNum">{{likeNum}}</span></div>
+    <!--<div class="blankClass"></div>-->
 
     <mt-navbar class="page-part" v-model="selected">
-      <mt-tab-item id="1">详细消息</mt-tab-item>
-      <mt-tab-item id="2">评论</mt-tab-item>
-      <mt-tab-item id="3">章节</mt-tab-item>
+      <mt-tab-item id="1">详情</mt-tab-item>
+      <mt-tab-item id="2">章节</mt-tab-item>
+      <mt-tab-item id="3">评价</mt-tab-item>
     </mt-navbar>
-
+    <div class="blankClass"></div>
     <mt-tab-container v-model="selected">
-
       <mt-tab-container-item id="1">
         <div class="detail-content">
-          <div class="detail-title">{{detailTitle}}</div>
+          <!--<div class="detail-title">{{detailTitle}}</div>-->
           <div class="detail-intro">
             <div class="detail-intro-title">课程介绍</div>
             <div class="detail-intro-content">{{introduce}}</div>
           </div>
         </div>
       </mt-tab-container-item>
-
       <mt-tab-container-item id="2">
-        <div class="comment-content-up">
-          <div class="comment-content">
-            <img class="user-img" :src="src" alt="" @error='changeImage($event)' style="float: left">
-            <textarea class="comment-area" placeholder="我来说两句..."></textarea>
-            <mt-button class="button" size="small">发布</mt-button>
-          </div>
-        </div>
-        <div class="total-comment"><span class="commentNum">{{commentNum}}</span>评论</div>
-        <div class="comment-content-down">
+        <ul>
+          <li v-for="sectionInfo in sectionList" class="section-li">
+            <div>
+              <p class="section">第{{sectionInfo.part}}章 <span>{{sectionInfo.title}}</span></p>
+            </div>
+          </li>
+        </ul>
+      </mt-tab-container-item>
 
+      <mt-tab-container-item id="3">
+
+        <div class="total-comment">
+          <!--<span class="commentNum">{{commentNum}}</span>评论-->
+          <mt-button class="edit-comment" @click.native="popupVisible1 = true" size="small" ref="button" >评 价</mt-button>
+        </div>
+
+        <mt-popup v-model="popupVisible1" popup-transition="popup-fade" class="mint-popup-1" :style="{ top: buttonBottom + 12 + 'rem' }">
+          <div class="comment-content-up">
+            <div class="comment-content">
+              <div class="close">x</div>
+              <div class="text-content">请输入您对该课件的评价</div>
+              <textarea class="comment-area"></textarea>
+              <div style="text-align: center"><mt-button class="button" size="small">提  交</mt-button></div>
+            </div>
+          </div>
+        </mt-popup>
+        <div class="comment-content-down">
           <ul>
             <li v-for="commentInfo in commentList" class="comment-li">
               <div>
@@ -43,29 +59,18 @@
                     <span class="day">{{commentInfo.day}}</span>
                     <span class="like">{{commentInfo.like}} 赞</span>
                   </p>
-                  </div>
+                </div>
               </div>
             </li>
           </ul>
         </div>
       </mt-tab-container-item>
-
-      <mt-tab-container-item id="3">
-        <ul>
-          <li v-for="sectionInfo in sectionList" class="section-li">
-            <div>
-                <p class="section">第{{sectionInfo.part}}章 <span>{{sectionInfo.title}}</span></p>
-            </div>
-          </li>
-        </ul>
-      </mt-tab-container-item>
-
     </mt-tab-container>
 
   </div>
 </template>
 <script>
-  import { Button, Cell, Navbar, TabItem, TabContainer, TabContainerItem } from 'mint-ui';
+  import { Button, Cell, Navbar, TabItem, TabContainer, TabContainerItem, Popup } from 'mint-ui';
   import Vue from 'vue';
   Vue.component(Button.name, Button);
   Vue.component(Cell.name, Cell);
@@ -73,11 +78,14 @@
   Vue.component(TabItem.name, TabItem);
   Vue.component(TabContainer.name, TabContainer);
   Vue.component(TabContainerItem.name, TabContainerItem);
+  Vue.component(Popup.name, Popup);
   export default{
     name:'detail',
     data(){
       return{
-        selected:'1'
+        selected:'1',
+        popupVisible1: false,
+        buttonBottom: 0
       }
     },
     props:['video','src','lecturerName','likeNum','detailTitle','introduce','commentNum','commentList','sectionList'],
@@ -88,38 +96,61 @@
         target.src="../static/images/lesson-default.svg"
         console.log(target.src)
       }
-    }
+    },
+//    mounted() {
+//      this.buttonBottom = this.$refs.button.$el.getBoundingClientRect().bottom;
+//    }
   }
 </script>
-<style>
+<style scoped>
   .detail-content, .comment-content, .comment, .username, .other, .total-comment, .section{
     text-align: left;
   }
-  .detail-intro-content, .other{
+  .detail-intro-content, .other, .text-content{
     color: #9d9d9d;
   }
-  .comment-li, .detail-title, .comment-content-up{
+  .comment-li, .comment-content-up{
     padding: 0.5rem;
   }
-  .detail-title{
-    font-size: 0.7rem;
+  .detail-intro-title, .comment-li, .section{
     border-bottom: 0.05rem #dddddd solid;
-    margin: 0 1rem 0.5rem;
   }
-  .detail-intro-title, .detail-intro-content{
-    font-size: 0.5rem;
+  .detail-intro-title, .detail-intro-content, .username, .other, .section, .detail-title, .likeNum, .comment, .text-content, .edit-comment{
+    font-size: 0.6rem;
+  }
+  .detail-title, .likeNum, .edit-comment{
+    color: rgb(0,205,135);
+  }
+  .button, .close{
+    background-color: rgb(0,205,135);
+  }
+  .clickLike{
+    height: 1rem;
+    padding: 0.5rem;
+    background-color: black;
+  }
+  .detail-title, .likeNum{
+    line-height: 1rem;
+  }
+  .detail-title{
+    float: left;
+  }
+  .likeNum{
+    float: right;
   }
   .detail-intro-title{
-    margin-left: 1.5rem;
+    margin: 1rem 1rem 0;
+    padding-bottom: 0.2rem;
   }
   .detail-intro-content{
-    margin: 1rem 1.5rem;
+    padding: 0.5rem 1rem;
   }
-  .comment-content-up{
-    border-bottom: 0.05rem solid #dddddd;
-  }
-  .comment-content-down{
-    /*padding: 0.5rem;*/
+  .edit-comment{
+    background: none;
+    border: none;
+    box-shadow: none;
+    width: 3rem;
+    margin-left: 12.5rem;
   }
   img{
     margin-right: 1rem;
@@ -132,32 +163,21 @@
     resize: none;
     width: 11.9rem;
     height: 4rem;
-
   }
   .button{
-    background-color: #26a2ff;
     color: #ffffff;
-    left: 12.4rem;
-  }
-  .total-comment{
-    border-bottom: 0.05rem solid #dddddd;
+    text-align: center;
+    width: 6rem;
   }
   .comment-li{
     list-style: none;
     height: 2.5rem;
-    border-bottom: 0.05rem solid #dddddd;
   }
   .comment-pull-right{
     float: left;
   }
   .commentNum{
     padding: 0 0.5rem;
-  }
-  .username, .other{
-    font-size: 0.5rem;
-  }
-  .comment{
-    font-size: 0.7rem;
   }
   .day{
     border-right: 0.05rem solid #dddddd;
@@ -167,10 +187,26 @@
     padding-left: 0.5rem;
   }
   .section{
-    border-bottom: 0.05rem solid #dddddd;
     margin: 0.5rem 0.5rem 0;
     padding-bottom: 0.5rem;
-    font-size: 0.5rem;
   }
-
+  .blankClass{
+    height: 0.5rem;
+    width: 100%;
+    background-color: #dddddd;
+  }
+  .close{
+    margin-left: 10.9rem;
+    text-align: center;
+    color: #ffffff;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 0.5rem;
+  }
+  .text-content{
+    margin-bottom: 0.5rem;
+  }
+  .page-part>a>div{
+    color: rgb(0,205,135);
+  }
 </style>
