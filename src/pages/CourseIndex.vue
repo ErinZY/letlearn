b4bea52969f6e2b420318fbd11678dbe6afb3c94
youxw5 <youxw5@yonyou.com>
2017-07-27 10:05:01 +0800

    课程首页修改

--- src/pages/CourseIndex.vue
+++ src/pages/CourseIndex.vue
@@ -3,10 +3,14 @@
     <sidebar v-if="isClickFilter" @closeit="closeMySidebar"></sidebar>
     <div class="course" v-if="!isClickFilter">
   <div class="my-header">
-    <my-header title="我的课程" :hasRight="hasRight" @filtrate="myfiltrate">
-    </my-header>
+    <!-- <my-header title="我的课程" :hasRight="hasRight" @filtrate="myfiltrate">
+    </my-header> -->
+      <router-link to="/Search"><div class="search"><img src="../assets/search.svg" alt=""></div></router-link>
+      <div class="filter" @click="myfiltrate"><img src="../assets/filter.svg" alt=""></div>
+      <div class="avatar"><img src="../assets/girl.svg" alt=""></div>
+      <div class="header-title">我的课程</div>
   </div>
-  <div class="search-input">
+  <!-- <div class="search-input">
     <div class="mint-search">
       <div class="mint-searchbar">
         <div class="mint-searchbar-inner">
@@ -15,7 +19,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
   <div class="course-list">
             <ul style="{height:wrapperHeight + 'px''}" ref ='wrapper'>
                 <loadmore ref='loadmore1'>
@@ -204,6 +208,50 @@ export default {
 </script>
 
 <style>
+.my-header{
+  text-align: center;
+color:#fff;
+position:relative;
+background: #06CF86;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to top, #93EDC7, #06CF86);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #93EDC7, #06CF86); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+}
+.search{
+margin:0.25rem;
+position:absolute;
+display: inline-block;
+/* float: left; */
+top:0;
+left: 0;
+}
+.search img{
+  width:0.75rem;
+  height:0.75rem;
+}
+.filter{
+  margin:0.25rem;
+  display: inline-block;
+  position:absolute;
+  top:0;
+  right:0;
+}
+.filter img{
+  width:0.75rem;
+  height:0.75rem;
+}
+.avatar img{
+  margin-top:1rem;
+  border-radius:100%;
+  display: inline-block;
+  /* background-color: #fff; */
+  /* line-height: 3rem; */
+  width:3rem;
+  height:3rem;
+}
+.header-title{
+  padding:0.5rem 0;
+}
 .mint-loadmore{
   width:100%;
 }
